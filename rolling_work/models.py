a8deaa18a193ecd2f9bcdeca1f9a10aa3c19fb91
Chuang Bing Cheng <tars3017@debian-BULLSEYE-live-builder-AMD64>
2022-07-06 15:28:19 +0800

    First try on heroku

--- rolling_work/models.py
+++ rolling_work/models.py
@@ -3,3 +3,15 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     pass
+
+class Record(models.Model):
+    owner = models.ForeignKey("User", on_delete=models.CASCADE)
+    
+    # app data (unit by seconds)
+    longest_work_period = models.IntegerField(default=0)
+    shortest_work_period = models.IntegerField(default=0)
+    longest_rest_period = models.IntegerField(default=0)
+    shortest_rest_period = models.IntegerField(default=0)
+    work_total = models.IntegerField(default=0)
+    app_total = models.IntegerField(default=0)
+    roll_count = models.IntegerField(default=0)

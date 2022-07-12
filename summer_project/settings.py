47b96b87ed2d5c595d0dea06093d5e0fe401ae0b
Chuang Bing Cheng <tars3017@debian-BULLSEYE-live-builder-AMD64>
2022-07-12 13:51:11 +0800

    Test for DEBUG=False

--- summer_project/settings.py
+++ summer_project/settings.py
@@ -40,8 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+import os
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #add whitenoise
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -49,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') ##specify static root
 
 ROOT_URLCONF = 'summer_project.urls'
 
